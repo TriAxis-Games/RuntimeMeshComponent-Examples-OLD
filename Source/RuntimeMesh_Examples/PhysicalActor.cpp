5bbc696d509745e97bd031cb3453f92e00dcd8ed
Chris Conway <chris@koderz.me>
2016-06-02 04:29:17 -0400

    Updated RMC to latest to include RTTI checking of vertex struct

--- Source/RuntimeMesh_Examples/PhysicalActor.cpp
+++ Source/RuntimeMesh_Examples/PhysicalActor.cpp
@@ -6,11 +6,65 @@
 
 
 
+
+	
+
+struct FMyCustomVertex 
+{
+	DECLARE_RUNTIMEMESH_VERTEXTYPEINFO_SIMPLE(FMyCustomVertex, FGuid(0x2CB42002, 0x83734382, 0xA3A03D07, 0x3896C852))
+
+	FMyCustomVertex() : Position(0.0f, 0.0f, 0.0f), Normal(FVector(0.0f, 0.0f, 1.0f)), Tangent(FVector(1.0f, 0.0f, 0.0f)), Color(255, 255, 255), TextureCoordinate(0, 0), TextureIndices(0, 0) { }
+
+	FMyCustomVertex(const FVector& Position, const FVector& Normal, const FVector& Tangent, FColor Color, FVector2DHalf TextureCoordinate, FVector2DHalf TextureIndices)
+		: Position(Position), Normal(Normal), Tangent(Tangent), Color(Color), TextureCoordinate(TextureCoordinate), TextureIndices(TextureIndices)
+	{
+		this->Normal.Vector.W = 255;
+	}
+
+	FMyCustomVertex(const FVector& Position, const FVector& Normal, const FVector& Tangent, bool bFlipTangentY, FColor Color, FVector2DHalf TextureCoordinate, FVector2DHalf TextureIndices)
+		: Position(Position), Normal(Normal), Tangent(Tangent), Color(Color), TextureCoordinate(TextureCoordinate), TextureIndices(TextureIndices)
+	{
+		this->Normal.Vector.W = bFlipTangentY ? 0 : 255;
+	}
+
+	/** Vertex position */
+	FVector Position;
+
+	/** Vertex normal */
+	FPackedNormal Normal;
+
+	/** Vertex tangent */
+	FPackedNormal Tangent;
+
+	/** Vertex texture co-ordinate */
+	FVector2DHalf TextureCoordinate;
+	FVector2DHalf TextureIndices;
+
+	/** Vertex color */
+	FColor Color;
+
+	static RuntimeMeshVertexStructure GetVertexStructure(const FRuntimeMeshVertexBuffer<FMyCustomVertex>& VertexBuffer)
+	{
+		RuntimeMeshVertexStructure VertexStructure;
+
+		VertexStructure.PositionComponent = RUNTIMEMESH_VERTEXCOMPONENT(VertexBuffer, FMyCustomVertex, Position, VET_Float3);
+		VertexStructure.TextureCoordinates.Add(RUNTIMEMESH_VERTEXCOMPONENT(VertexBuffer, FMyCustomVertex, TextureCoordinate, VET_Half4));
+		VertexStructure.TangentBasisComponents[0] = RUNTIMEMESH_VERTEXCOMPONENT(VertexBuffer, FMyCustomVertex, Tangent, VET_PackedNormal);
+		VertexStructure.TangentBasisComponents[1] = RUNTIMEMESH_VERTEXCOMPONENT(VertexBuffer, FMyCustomVertex, Normal, VET_PackedNormal);
+		VertexStructure.ColorComponent = RUNTIMEMESH_VERTEXCOMPONENT(VertexBuffer, FMyCustomVertex, Color, VET_Color);
+
+		return VertexStructure;
+	}
+
+};
+DEFINE_RUNTIMEMESH_VERTEXTYPEINFO(FMyCustomVertex)
+
+
+
 void GenerateBoxMesh(const FVector& Size, TArray<MyVertex>& Vertices, TArray<FVector>& VertexPositions, TArray<MyDualVertex>& VertexData, TArray<int32>& Triangles)
 {
 	Vertices.Empty();
 	Triangles.Empty();
-
 
 	TArray<FVector> Positions;
 	TArray<FVector> Normals;
